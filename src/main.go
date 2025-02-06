a6e049781b0f0dad4c43b604278d9e59dcc7b945
vdvivek <vdvivekdenny@gmail.com>
2025-02-06 12:38:47 +0000

    setup APIs

---
+++ src/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"gpsd-user-mgmt/src/user"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/list", user.List)
+		v1.POST("/create", user.Create)
+
+		v1.GET("/edit", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"API": "edit",
+			})
+		})
+
+		v1.GET("/signin", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"API": "signin",
+			})
+		})
+
+		v1.GET("/signout", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"API": "signout",
+			})
+		})
+	}
+
+	return router
+}
+
+func main() {
+	router := setupRouter()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("Unable to read port")
+		return
+	}
+	address := fmt.Sprintf(":%s", port)
+
+	router.Run(address)
+}
--- src/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/api/user/health")
-def get_sensor_health():
-    return {"status": "healthy"}
