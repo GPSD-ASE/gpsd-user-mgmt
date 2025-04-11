dcdacee1e4bad4f02f7f2f654d5e6d2570ec0f5c
Prakash Narasimhan <45827034+Prakash3008@users.noreply.github.com>
2025-04-11 14:49:06 +0100

    fix: Update payloads.go

--- src/user/payloads.go
+++ src/user/payloads.go
@@ -1,6 +1,6 @@
 package user
 
 type signInRequest struct {
-	Username string `json"username"`
-	Password string `json"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
